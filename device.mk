4aaa54ca096b9fe1514d0da59a2a2b645f0f65af
rdxzv <rdxzv.dev@gmail.com>
2023-07-11 23:09:38 +0000

    surya: import XiaomiParts
    - from https://github.com/rd-stuffs/android_device_xiaomi_surya
    - history https://github.com/rd-stuffs/android_device_xiaomi_surya/tree/topaz-wip/parts
    - also import misoundfx libs
    
    Signed-off-by: Hazama25 <hazamafawkes@gmail.com>

--- device.mk
+++ device.mk
@@ -507,3 +507,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.wifi.passpoint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.passpoint.xml \
     frameworks/native/data/etc/android.hardware.wifi.rtt.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.rtt.xml \
     frameworks/native/data/etc/android.hardware.wifi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.xml
+
+# XiaomiParts
+$(call inherit-product, device/xiaomi/surya/parts/parts.mk)
+
